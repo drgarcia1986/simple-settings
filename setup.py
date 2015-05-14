1e08400d1aaaf9213b8a2455cab8fb5ea55115e0
Diego Garcia <drgarcia1986@gmail.com>
2015-05-14 00:18:43 -0300

    simplify setup file

--- setup.py
+++ setup.py
@@ -1,40 +1,21 @@
 # -*- coding: utf-8 -*-
-import os
 from setuptools import setup
 
 
 def read(fname):
     """ Return file content. """
+    with open(fname) as f:
+        content = f.read()
 
-    return open(
-        os.path.join(
-            os.path.dirname(__file__), fname)
-    ).read()
-
-
-def get_packages(package):
-    """ Return root package and all sub-packages. """
-
-    return [dirpath
-            for dirpath, dirnames, filenames in os.walk(package)
-            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+    return content
 
 
-def get_package_data(package):
-    """
-    Return all files under the root package,
-    that are not in a package themselves.
-    """
+description = 'A simple way to manage your project settings'
+try:
+    long_description = read('README.md')
+except:
+    long_description = description
 
-    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
-            for dirpath, dirnames, filenames in os.walk(package)
-            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-    filepaths = []
-    for base, filenames in walk:
-        filepaths.extend([os.path.join(base, filename)
-                          for filename in filenames])
-    return {package: filepaths}
 
 setup(
     name='simple-settings',
@@ -44,10 +25,11 @@ setup(
     author='Diego Garcia',
     author_email='drgarcia1986@gmail.com',
     keywords='django flask bottle tornado settings configuration conf',
-    description='A simple way to manage your project settings',
-    long_description=read('README.md'),
-    packages=get_packages('simple_settings'),
-    packages_data=get_package_data('simple_settings'),
+    description=description,
+    long_description=long_description,
+    download_url='https://github.com/drgarcia1986/drgarcia1986/tarball/master',
+    packages=['simple_settings'],
+    package_dir={'simple_settings': 'simple_settings'},
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
