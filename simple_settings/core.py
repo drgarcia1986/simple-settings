9a4c944560c92ee122e084036cfa7587c7e85063
Diego Garcia <drgarcia1986@gmail.com>
2015-05-25 23:23:26 -0300

    remove unnecessary settings_module argument

--- simple_settings/core.py
+++ simple_settings/core.py
@@ -15,7 +15,7 @@ class _Settings(object):
             self._settings_module = os.environ.get('settings')
         if not self._settings_module:
             raise RuntimeError('Settings are not configured')
-        self._load_settings_module(self._settings_module)
+        self._load_settings_module()
 
     def _get_settings_from_cmd_line(self):
         for a in sys.argv:
@@ -25,8 +25,8 @@ class _Settings(object):
                 except IndexError:
                     return None
 
-    def _load_settings_module(self, settings_module):
-        module = importlib.import_module(settings_module)
+    def _load_settings_module(self):
+        module = importlib.import_module(self._settings_module)
         for setting in dir(module):
             value = os.environ.get(setting, getattr(module, setting))
             self._dict[setting] = value
