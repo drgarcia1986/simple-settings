d6f423964856a57cbae70e60280ebbf05293502f
Diego Garcia <drgarcia1986@gmail.com>
2015-05-31 13:13:21 -0300

    refactor in python module strategy

--- simple_settings/strategies/python.py
+++ simple_settings/strategies/python.py
@@ -1,21 +1,28 @@
 # -*- coding: utf-8 -*-
 import importlib
 import os
-from simple_settings.strategies.types import SettingsStrategy
+from .types import SettingsStrategy
 
 
 def _is_python_module(file_name):
-    return not file_name.endswith('.cfg')  # FIXME
+    try:
+        importlib.import_module(file_name)
+        return True
+    except ImportError:
+        return False
 
 
-def _load_python_module(settings_module):
+def _load_python_module(settings_file):
     result = {}
-    module = importlib.import_module(settings_module)
+    module = importlib.import_module(settings_file)
     for setting in dir(module):
         value = os.environ.get(setting, getattr(module, setting))
         result[setting] = value
     return result
 
+
 strategy = SettingsStrategy(
-    'python', _is_python_module, _load_python_module
+    name='python',
+    is_valid_file=_is_python_module,
+    load_settings_file=_load_python_module
 )
