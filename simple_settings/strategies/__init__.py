167dd98de90b7ee552ad0ffd1bd3687f23cdf364
Diego Garcia <drgarcia1986@gmail.com>
2016-01-30 19:10:37 -0200

    create strategy to load yaml files

--- simple_settings/strategies/__init__.py
+++ simple_settings/strategies/__init__.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from .cfg import SettingsLoadStrategyCfg
 from .python import SettingsLoadStrategyPython
+from .yaml_file import SettingsLoadStrategyYaml
 
 
 strategies = (
     SettingsLoadStrategyPython,
-    SettingsLoadStrategyCfg
+    SettingsLoadStrategyCfg,
+    SettingsLoadStrategyYaml
 )
