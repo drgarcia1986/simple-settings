ccf3ea9ea551aa795948e643deb1ca037672cb7e
Michael Mejia <micmejia@inetvators.com>
2019-07-06 23:29:48 +0800

    allow the environment variables to be used as a virtual settings file

--- simple_settings/strategies/__init__.py
+++ simple_settings/strategies/__init__.py
@@ -2,6 +2,7 @@
 from .cfg import SettingsLoadStrategyCfg
 from .json_file import SettingsLoadStrategyJson
 from .python import SettingsLoadStrategyPython
+from .environ import SettingsLoadStrategyEnviron
 
 yaml_strategy = None
 try:
@@ -21,7 +22,8 @@ except ImportError:  # pragma: no cover
 strategies = (
     SettingsLoadStrategyPython,
     SettingsLoadStrategyCfg,
-    SettingsLoadStrategyJson
+    SettingsLoadStrategyJson,
+    SettingsLoadStrategyEnviron
 )
 
 if yaml_strategy:
---
+++ simple_settings/strategies/environ.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+
+class SettingsLoadStrategyEnviron(object):
+    """
+    This is the strategy used to read settings from `os.environ`.
+    `file_name` could be '.environ' to load all environment variables or
+    '`PREFIX`.environ' to load environment variables starting with `PREFIX`. 
+    """
+    name = 'environ'
+
+    @staticmethod
+    def is_valid_file(file_name):
+        return file_name.endswith('.environ')
+
+    @staticmethod
+    def load_settings_file(settings_file):
+        env_prefix = settings_file.replace('.environ', '')
+        if env_prefix:
+            result = {}
+            for k, v in os.environ.items():
+                if k.startswith(env_prefix):
+                    result[k] = v
+        else:
+            result = os.environ.copy()
+        return result
